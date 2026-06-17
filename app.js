41da8895350af4859b5ba171ecdbd94af03173e9
Ace <kvnallenn@gmail.com>
2026-06-17 17:10:27 +0700

    debug token

--- app.js
+++ app.js
@@ -1,58 +1,46 @@
-const axios = require('axios');
+const axios = require("axios");
 
 const TOKEN = process.env.TOKEN;
 const GAS_URL = process.env.GAS_URL;
 
 const API_URL =
-  'https://apimenyala.robuxindo.com/api/admin-orders?productType=VILOG&page=1&perPage=20&sortField=createdAt&sortOrder=desc&status=PENDING&paymentStatus=SUCCESS';
+  "https://apimenyala.robuxindo.com/api/admin-orders?productType=VILOG&page=1&perPage=20&sortField=createdAt&sortOrder=desc&status=PENDING&paymentStatus=SUCCESS";
 
 async function checkOrders() {
   try {
-    console.log('Checking orders...');
+    console.log("Checking orders...");
 
     const res = await axios.get(API_URL, {
       headers: {
-        Authorization: `Bearer ${TOKEN}`
-      }
+        Authorization: `Bearer ${TOKEN}`,
+      },
     });
 
-    const items =
-      res.data?.data?.items || [];
+    const items = res.data?.data?.items || [];
 
     for (const order of items) {
-      await axios.post(
-        GAS_URL,
-        {
-          id: order.id,
-          orderNumber:
-            order.orderNumber,
-          username:
-            order.username,
-          robux:
-            order.robux,
-          totalAmount:
-            order.totalAmount,
-          customerEmail:
-            order.customerEmail,
-          customerPhone:
-            order.customerPhone,
-          status:
-            order.status,
-          paymentStatus:
-            order.paymentStatus,
-          createdAt:
-            order.createdAt
-        }
-      );
+      await axios.post(GAS_URL, {
+        id: order.id,
+        orderNumber: order.orderNumber,
+        username: order.username,
+        robux: order.robux,
+        totalAmount: order.totalAmount,
+        customerEmail: order.customerEmail,
+        customerPhone: order.customerPhone,
+        status: order.status,
+        paymentStatus: order.paymentStatus,
+        createdAt: order.createdAt,
+      });
 
-      console.log(
-        'Sent:',
-        order.orderNumber
-      );
+      console.log("Sent:", order.orderNumber);
     }
   } catch (err) {
     console.error(err.message);
   }
 }
+
+console.log("TOKEN ADA:", !!process.env.TOKEN);
+
+console.log("PANJANG TOKEN:", process.env.TOKEN?.length);
 
 checkOrders();
